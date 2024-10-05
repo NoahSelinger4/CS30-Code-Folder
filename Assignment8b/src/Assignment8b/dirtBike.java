c474f10523cf1d7a6384f3d5ec90244d456da478
NoahSelinger4 <noah.selinger@rcsd.ca>
2024-10-05 14:04:08 -0600

    hw

---
+++ Assignment8b/src/Assignment8b/dirtBike.java
@@ -0,0 +1,21 @@
+//Program Name:dirtBike
+//Student Name:Noah Selinger
+//Date:October 5th 2024
+//Program Description: A program that calls two methods from a dirtbike class
+
+package Assignment8b;
+
+public class dirtBike {
+    
+    public void rev() 
+    {
+        System.out.println("vroom vroom");
+
+    }
+    public void wheelie() 
+    {
+        System.out.println("This is awesome!");
+
+    }
+    
+}
---
+++ Assignment8b/src/Assignment8b/dirtBikeRunner.java
@@ -0,0 +1,21 @@
+//Program Name:dirtBike
+//Student Name:Noah Selinger
+//Date:October 5th 2024
+//Program Description: A program that calls two methods from a dirtbike class
+
+package Assignment8b;
+
+public class dirtBikeRunner {
+
+    public static void main(String[] args)
+    {
+        dirtBike race = new dirtBike();
+        race.rev();
+        race.wheelie();
+        race.rev();
+        race.wheelie();
+        race.rev();
+
+    }
+
+}
